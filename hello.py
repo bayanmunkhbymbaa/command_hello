f4139bce3c3397e89ca79c6584de52cbaadb6f54
bymbaa <bymbajargal0328@gmail.com>
2022-03-31 11:04:26 +0900

    hello.py modified on Local repository

--- hello.py
+++ hello.py
@@ -1,14 +1,8 @@
 // For command line git tutorial
-<<<<<<< HEAD
-// Local repository modification 
-=======
-// GitHub modification 
->>>>>>> origin/master
+// Local repository modification
+
 
 print("Hello world")
 print("Tell your world")
-<<<<<<< HEAD
 print("Tell his  world")
-=======
 print("Tell her world")
->>>>>>> hotfix
