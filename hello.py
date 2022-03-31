85f1e3b850286621aa94374fa94aa385a4063820
bayanmunkhbymbaa <102271653+bayanmunkhbymbaa@users.noreply.github.com>
2022-03-31 10:51:14 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
+// For command line git tutorial
+// First: Github modification
+// Second: Local repository modification 
+
 print("Hello world")
 print("Tell your world")
-<<<<<<< HEAD
 print("Tell his  world")
-=======
 print("Tell her world")
->>>>>>> hotfix
+print("Tell my world")
